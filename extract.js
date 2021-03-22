982d264a9efeba3ee4e1e62183c792a64038054d
Artie <arturaszuta@gmail.com>
2021-03-21 23:58:28 -0400

    new kata

---
+++ extract.js
@@ -0,0 +1,29 @@
+function domainName(url){
+    //your code here
+      let firstOccurance;
+      let lastOccurance
+      let answer;
+      const wwwIndex = url.indexOf("www");
+      const slashIndex = url.indexOf("//");
+      console.log(wwwIndex);
+      console.log(slashIndex);
+      
+      
+      
+      if (wwwIndex < 0 && slashIndex < 0) {
+          firstOccurance = url.indexOf(".");
+          answer = url.substring(0, firstOccurance);
+      } else if (wwwIndex < 0) { 
+            firstOccurance = url.indexOf("//");
+            lastOccurance = url.indexOf(".");
+            answer = url.substring(firstOccurance + 2, lastOccurance);
+      } else {
+          firstOccurance = url.indexOf(".");
+          lastOccurance =  url.indexOf(".", firstOccurance + 1);
+          answer = url.substring(firstOccurance + 1, lastOccurance);
+      }
+      return answer;
+  }
+
+  console.log(domainName("http://google.com"));
+  console.log(domainName("google.com"));
